5a230f8a0afb5e3f4ef0c2b590ac81a258c0eae8
Michael Kroon <kroonology@gmail.com>
2019-08-29 15:34:49 -0600

    add priority col to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_211558) do
+ActiveRecord::Schema.define(version: 2019_08_29_213251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_08_29_211558) do
     t.bigint "task_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "priority"
     t.index ["task_id"], name: "index_tasks_on_task_id"
   end
 
