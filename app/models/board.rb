188b971e96f294afc507752ed08d13466ea445f9
Michael Kroon <kroonology@gmail.com>
2019-09-03 00:07:39 -0600

    Create dependency relationship to allow for deleting board. Create SQL for selecting all boards.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,11 @@
 class Board < ApplicationRecord
-  has_many :lists
+  has_many :lists, dependent: :destroy
+
+  def self.all_boards
+    Board.find_by_sql(
+      "SELECT *
+      FROM boards"
+    )
+  end
 
 end
